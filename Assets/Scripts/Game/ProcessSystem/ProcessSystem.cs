a7ba902d00a31cc6bde9cb0dbb49e0d75499b762
dugite-git <tubasa283one@gmail.com>
2025-04-15 21:13:44 +0900

    ProcessSystemのクラスを作った
    なにもないけど

---
+++ Assets/Scripts/Game/ProcessSystem/ProcessSystem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace App.Game.ProcessSystem
+{
+    public class ProcessSystem : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+        void FixedUpdate()
+        {
+            
+        }
+    }
+}
