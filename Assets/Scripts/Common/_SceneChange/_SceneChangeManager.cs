818564dd93b6f2789c1c84fffcdd788f94541371
dugite-git <tubasa283one@gmail.com>
2025-04-18 21:05:53 +0900

    データの受け渡しシステムを作った

--- Assets/Scripts/Common/_SceneChange/_SceneChangeManager.cs
+++ Assets/Scripts/Common/_SceneChange/_SceneChangeManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using App.Title.Camera;
 
 namespace App.Common._SceneChange
 {
@@ -19,32 +20,32 @@ namespace App.Common._SceneChange
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
         }
-        static public void ChengeSceneToResult(EndName endName)
+        static public void ChengeSceneToResult(EndName endName, int finalActiveUserNumber, int maxActiveUserNumber)
         {
-            if (endName == EndName.BadEnd)
-            {
-                SceneManager.sceneLoaded += (scene, mode) => ResultSceneLoaded(endName);
-                UnityEngine.SceneManagement.SceneManager.LoadScene("Result");
-            }
-            else if (endName == EndName.GoodEnd)
-            {
-                SceneManager.sceneLoaded += (scene, mode) => ResultSceneLoaded(endName);
-                UnityEngine.SceneManagement.SceneManager.LoadScene("Result");
-            }
-            else if (endName == EndName.NormalEnd)
-            {
-                SceneManager.sceneLoaded += (scene, mode) => ResultSceneLoaded(endName);
-                UnityEngine.SceneManagement.SceneManager.LoadScene("Result");
-            }
+            SceneManager.sceneLoaded += (scene, mode) => ResultSceneLoaded(endName, finalActiveUserNumber, maxActiveUserNumber);
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Result");
         }
 
-        static private void ResultSceneLoaded(EndName endName)
+        static private void ResultSceneLoaded(EndName endName, int finalActiveUserNumber, int maxActiveUserNumber)
         {
-            // Resultシーンが読み込まれた後の処理をここに記述
-            // 例えば、UIの初期化やデータの設定など
-            // ここでは例としてDebug.Logを使用していますが、実際には必要な処理を実装してください
+            // ResultDataHolderを取得
+            var resultDataHolder = GameObject.Find("ResultDataHolder");
+            if (resultDataHolder != null)
+            {
+                var resultData = resultDataHolder.GetComponent<ResultDataHolder>();
+                if (resultData != null)
+                {
+                    resultData.finalActiveUserNumber = finalActiveUserNumber;
+                    resultData.maxActiveUserNumber = maxActiveUserNumber;
+                    resultData.endName = endName;
+                }
+            }
+            else
+            {
+                Debug.LogError("ResultDataHolderが見つかりません。");
+            }
             Debug.Log("Resultシーンが読み込まれました。");
-            SceneManager.sceneLoaded -= (scene, mode) => ResultSceneLoaded(endName);
+            SceneManager.sceneLoaded -= (scene, mode) => ResultSceneLoaded(endName, finalActiveUserNumber, maxActiveUserNumber);
         }
     }
 }
