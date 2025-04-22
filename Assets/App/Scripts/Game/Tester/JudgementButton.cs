87c498396be8e8f6f9209783cbe13d17a2447bb3
dugite-git <138967966+dugite-git@users.noreply.github.com>
2025-04-22 15:08:24 +0900

    スクリプトの実装完了

--- Assets/App/Scripts/Game/Tester/JudgementButton.cs
+++ Assets/App/Scripts/Game/Tester/JudgementButton.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using UnityEngine;
+using TMPpro;
 using App.Game.ProcessSystem;
 using App.Scripts.Game.Profile;
 using App.Scripts.Game.Documents;
@@ -8,7 +9,7 @@ namespace App.Scripts.Game.Tester
 {
     public class JudgementButton : MonoBehaviour
     {
-        [SerializeField] private GameObject JudgementText;
+        [SerializeField] private TextMeshProUGUI JudgementText;
         [SerializeField] private ProcessSystem _processSystem;
         private GameObject[] profileList;
         void Start()
@@ -16,17 +17,42 @@ namespace App.Scripts.Game.Tester
             JudgementText.SetActive(false);
             profileList = _processSystem.GetProfileList();
         }
-        
+
         public void JudgementButtonClick()
         {
             foreach (var profile in profileList)
             {
-                if(profile.GetComponent<DocumentsAnimation>().isInCamera)
+                if (profile.GetComponent<DocumentsAnimation>().isInCamera)
                 {
-                    JudgementText.SetActive(true);
-                    
+                    StartCoroutine(DetectTextAnimation(profile));
                 }
             }
+        }
+
+        private IEnumerator DetectTextAnimation(GameObject profile)
+        {
+            JudgementText.SetActive(true);
+            JudgementText.text = "検出中";
+            yield return new WaitForSeconds(0.5f);
+            JudgementText.text = "検出中.";
+            yield return new WaitForSeconds(0.5f);
+            JudgementText.text = "検出中..";
+            yield return new WaitForSeconds(0.5f);
+            JudgementText.text = "検出中...";
+            yield return new WaitForSeconds(0.5f);
+            JudgementText.text = "検出完了";
+            yield return new WaitForSeconds(0.5f);
+            if(profile.ProfilePrefab.IsPhotoEffect())
+            {
+                JudgementText.text = "加工有"
+            }
+            else
+            {
+                JudgementText.text = "加工無";
+            }
+            yield return new WaitForSeconds(4f);
+            JudgementText.SetActive(false);
+            JudgementText.text = "";
         }
     }
 
